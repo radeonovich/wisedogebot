330a467b9eb5beecf71ca3d3ba4d4b8722f3c866
radeonovich <radeonovich1@yandex.ru>
2022-06-19 13:57:50 +0300

    replace adminid with the list of moderators id's move button suggest/moderate to the new function makebuttons add visual buttons of accept/deny/skip image while moderating add message if moderation queue is empty add message getting image from user change "open" to "with open as" code cleanup and refactor code commenting

--- main.py
+++ main.py
@@ -2,44 +2,43 @@ import telebot
 from telebot import types
 import logging
 
-logging.basicConfig(level=logging.DEBUG)  # set logger to log all info
+logging.basicConfig(level=logging.info)  # set logger to log all info except telegram debug messages
 
-tokenFile = open("token.txt", 'r')
-bot = telebot.TeleBot(tokenFile.read())
-tokenFile.close()
-
-channelName = '@testomeska'
-adminid = 518283574
+with open("token.txt", 'r') as tokenFile:  # get bot token
+    bot = telebot.TeleBot(tokenFile.read())
+channelName = '@testomeska'  # channel to post to
+moderators = [518283574]  # who can moderate
 
 
 def checkadmin(message):
-    if message.from_user.id == adminid:
+    if message.from_user.id in moderators:
         return True
     return False
 
-#def moderate(message, i):
-    # with open("moderationQueue.txt", "r") as modQueueFile:
-    #     imglist = modQueueFile.readlines()
-    #
-    # datalist = imglist[i].split()
-    # bot.send_photo(message.chat.id, photo=datalist[0])
-    # bot.send_message(message.chat.id, 'Картинка от {0} id{1}'.format(datalist[1].username, datalist[1]))
 
 @bot.message_handler(commands=["start"])
-def start(message, res=False):
+def start(message):
     logging.info("User {0}, id{1} entered /start".format(message.from_user.first_name, str(message.from_user.id)))
+    markup = makebuttons(message)
+    bot.send_message(message.chat.id,
+                     'Здесь ты можешь предложить опубликовать свою мудрость Клыка.\nВыбери режим работы.',
+                     reply_markup=markup)  # hello message
+
+
+imglist = [] # don't know how to avoid global variables here
+datalist = []
+
+
+def makebuttons(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Предложить")
     markup.add(item1)
     if checkadmin(message):
         item2 = types.KeyboardButton("Модерировать")
         markup.add(item2)
-    bot.send_message(message.chat.id,
-                     'Здесь ты можешь предложить опубликовать свою мудрость Клыка.\nВыбери режим работы.',
-                     reply_markup=markup)
+    return markup
 
-imglist=[]
-datalist=[]
+
 @bot.message_handler(content_types=["text"])
 def handle_admin_text(message):
     global imglist, datalist
@@ -48,63 +47,66 @@ def handle_admin_text(message):
 
     if message.text.strip() == 'Модерировать':
         if checkadmin(message):
-            bot.send_message(message.chat.id, 'Включен режим модерации.')
             logging.warning("User {0}, id{1} started moderating".format(message.from_user.first_name, str(message.from_user.id)))
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # add buttons for moderation
             item1 = types.KeyboardButton("+")
             item2 = types.KeyboardButton("-")
             item3 = types.KeyboardButton("Пропуск")
-            # with open("moderationQueue.txt", "r+") as modQueueFile:
-            #     new_file = modQueueFile.readlines()
-            #     modQueueFile.seek(0)
-            #     for line in new_file:
-            #         dataList = line.split()  # split line by spaces: dataList[0] = image id, [1] = user id
-            #         bot.send_photo(message.chat.id, photo=dataList[0])
-            #         bot.send_message(message.chat.id, 'Картинка от {0} id{1}'.format(dataList[1].username, dataList[1]))
-            #     modQueueFile.truncate()
+            markup.add(item1)
+            markup.add(item2)
+            markup.add(item3)
             with open("moderationQueue.txt", "r") as modQueueFile:
-                imglist = modQueueFile.readlines()
+                imglist = modQueueFile.readlines()  # list of image data, each element has format: image_id user_id
             if len(imglist) != 0:
+                bot.send_message(message.chat.id, 'Включен режим модерации.')
                 datalist = imglist[0].split()
+                # take the last image data and split to [image_id, user_id]
+                # maybe dict would be better here
                 bot.send_photo(message.chat.id, photo=datalist[0])
-                bot.send_message(message.chat.id, 'Картинка от {0} id{1}'.format('username', datalist[1]))
-                bot.send_message(message.chat.id, 'Модерируй, хуле. ', reply_markup=markup)
-                #moderate(message, 0)
+                bot.send_message(message.chat.id, 'Картинка от {0}id{1}'.format('', datalist[1]), reply_markup=markup)
+            else:
+                bot.send_message(message.chat.id, 'Предложка пуста.')
         else:
             bot.send_message(message.chat.id, 'Вы не можете модерировать.')
-    if checkadmin(message) and len(imglist) != 0:
+    if checkadmin(message) and len(imglist) != 0:  # handling moderator decision
+
         if message.text.strip() == '+':
             postQueueFile = open("postQueue.txt", "a")
             postQueueFile.write(datalist[0]+'\n')
             postQueueFile.close()
             imglist.pop(0)
-            bot.send_message(message.chat.id, 'Картинка добавлена в очередь, image id=' + datalist[0])
             with open("moderationQueue.txt", "w") as modQueueFile:
                 for line in imglist:
                     modQueueFile.write(line)
+            markup = makebuttons(message)
+            bot.send_message(message.chat.id, 'Картинка добавлена в очередь, image id=' + datalist[0], reply_markup=markup)
+
         elif message.text.strip() == '-':
             imglist.pop(0)
             with open("moderationQueue.txt", "w") as modQueueFile:
                 for line in imglist:
                     modQueueFile.write(line)
+            markup = makebuttons(message)
+            bot.send_message(message.chat.id, 'Картинка снята с модерации.', reply_markup=markup)
+
         elif message.text.strip() == 'Пропуск':
-            imglist.append(imglist.pop(0))
+            imglist.append(imglist.pop(0))  # move image to the start of list
             with open("moderationQueue.txt", "w") as modQueueFile:
                 for line in imglist:
                     modQueueFile.write(line)
-@bot.message_handler(content_types=['photo'])
+            markup = makebuttons(message)
+            bot.send_message(message.chat.id, 'Картинка перемещена в начало очереди.', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['photo'])  # receive an image from user
 def handle_photo(message):
-    receivedImages = []
-    # for photo in message.photo:
-    #     receivedImages.append(photo.file_id)
-    receivedImages.append(message.photo[-1].file_id)
+    received_image = message.photo[-1].file_id
     logging.info(
         'A photo has just received from user {0}, id {1}'.format(message.from_user.first_name, message.from_user.id))
-    for image in receivedImages:
-        modQueueFile = open("moderationQueue.txt", "a")
-        modQueueFile.write(image + ' ' + str(message.from_user.id) + '\n')
+    with open("moderationQueue.txt", "a") as modQueueFile:
+        modQueueFile.write(received_image + ' ' + str(message.from_user.id) + '\n')
         modQueueFile.close()
-        #bot.send_photo(message.chat.id, photo=image)
+    bot.send_message(message.chat.id, 'Отправил на проверку.')
 
 
 bot.polling(none_stop=True, interval=0)
