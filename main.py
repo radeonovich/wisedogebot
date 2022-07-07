0d700a69db5afb9dc2b77758c229d43dbea53d9f
radeonovich <radeonovich1@yandex.ru>
2022-07-07 12:02:58 +0300

    code beautify

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from sys import exit
 import logging
 import sqlite3
 import configparser
+
 config = configparser.ConfigParser()
 if Path("./config.ini").is_file():
     config.read("./config.ini")
 else:
     logging.basicConfig(level=logging.INFO)  # set logger to log all info except telegram debug messages
 
-    config = configparser.ConfigParser() # init config file
+    config = configparser.ConfigParser()  # init config file
 
     config.add_section('main')
     config.set('main', 'token', '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
@@ -29,15 +30,17 @@ else:
     exit(0)
 
 
-token = config.get('main', 'token') # get bot token
+token = config.get('main', 'token')  # get bot token
 bot = telebot.TeleBot(token)
-channelName = config.get('main', 'channelName') # channel to post to
-moderators = config.get('main', 'moderators').split() # who can moderate
-try: # ascii greeting in console
-    from art import * # pip install art
+channelName = config.get('main', 'channelName')  # channel to post to
+moderators = config.get('main', 'moderators').split()  # who can moderate
+try:  # ascii greeting in console
+    from art import *  # pip install art
     tprint('WiseDogeBot')
 except ImportError:
     pass
+
+
 def sqlite_connect():
     conn = sqlite3.connect("db/database.db", check_same_thread=False)
     conn.execute("pragma journal_mode=wal;")
@@ -70,21 +73,25 @@ except FileNotFoundError:
         logging.info("Created database successfully.")
 
 
-def insertqueue(table:str, user_id:int, username:str, image:str, extra:str): # adds an element to some queue (suggestion or post)
+def insert_queue(table: str, user_id: int, username: str, image: str, extra: str):
+    # adds an element to suggestion or post queue
     conn = sqlite_connect()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO ' + table + ' (user_id, username, image, extra) VALUES (?,?,?,?)', (user_id, username, image, extra))
+    cursor.execute(
+        'INSERT INTO ' + table + ' (user_id, username, image, extra) VALUES (?,?,?,?)',
+        (user_id, username, image, extra)
+    )
     conn.commit()
 
 
-def popqueue(table:str, image:str):
+def pop_queue(table: str, image: str):
     conn = sqlite_connect()
     cursor = conn.cursor()
     cursor.execute('DELETE FROM ' + table + ' WHERE image = ?', (image,))
     conn.commit()
 
 
-def checkadmin(message):
+def check_admin(message):
     if str(message.from_user.id) in moderators:
         return True
     return False
@@ -93,17 +100,18 @@ def checkadmin(message):
 @bot.message_handler(commands=["start"])
 def start(message):
     logging.info("User {0}, id{1} entered /start".format(message.from_user.first_name, str(message.from_user.id)))
-    markup = makebuttons(message)
-    bot.send_message(message.chat.id,
-                     'Здесь ты можешь предложить опубликовать свою мудрость Клыка.\nВыбери режим работы.',
-                     reply_markup=markup)  # hello message
+    markup = make_buttons(message)
+    bot.send_message(
+        message.chat.id,
+        'Здесь ты можешь предложить опубликовать свою мудрость Клыка.\nВыбери режим работы.',
+        reply_markup=markup)  # hello message
 
 
-def makebuttons(message):
+def make_buttons(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Предложить")
     markup.add(item1)
-    if checkadmin(message):
+    if check_admin(message):
         item2 = types.KeyboardButton("Модерировать")
         markup.add(item2)
     return markup
@@ -116,8 +124,10 @@ def handle_admin_text(message):
         bot.send_message(message.chat.id, 'Пришли картинку с волком, я добавлю ее в очередь модерации.')
 
     if message.text.strip() == 'Модерировать':
-        if checkadmin(message):
-            logging.warning("User {0}, id{1} started moderating".format(message.from_user.first_name, str(message.from_user.id)))
+        if check_admin(message):
+            logging.warning("User {0}, id{1} started moderating".format(
+                message.from_user.first_name, str(message.from_user.id))
+            )
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # add buttons for moderation
             item1 = types.KeyboardButton("+")
             item2 = types.KeyboardButton("-")
@@ -129,39 +139,67 @@ def handle_admin_text(message):
             conn = sqlite_connect()
             cursor = conn.cursor()
             try:
-                row = cursor.execute(\
-                    'SELECT user_id, username, image, extra FROM SuggestionQueue ORDER BY id ASC LIMIT 1'\
-                    ).fetchall()[0] # get last image from suggested
+                row = cursor.execute(
+                    'SELECT user_id, username, image, extra FROM SuggestionQueue ORDER BY id ASC LIMIT 1'
+                    ).fetchall()[0]  # get last image from suggested
             except IndexError:
                 bot.send_message(message.chat.id, 'Предложка пуста.')
             else:
-                user_id = row[0] # bad practice, better replace to a dictionary
+                user_id = row[0]  # bad practice, better replace to a dictionary
                 username = row[1]
                 image = row[2]
                 extra = row[3]
                 bot.send_photo(message.chat.id, photo=image)
-                bot.send_message(message.chat.id, 'Картинка от {0} id{1}'.format(username, user_id), reply_markup=markup)
+                bot.send_message(
+                    message.chat.id,
+                    'Картинка от {0} id{1}'.format(username, user_id),
+                    reply_markup=markup
+                )
 
         else:
             bot.send_message(message.chat.id, 'Вы не можете модерировать.')
     # handling moderator decision
-    if checkadmin(message):
+    if check_admin(message):
 
-        if message.text.strip() == '+': # will make bug if moderator sent + without the context of moderating
-            insertqueue(table='PostQueue', user_id=user_id, username=username, image=image, extra=extra)
-            markup = makebuttons(message)
-            bot.send_message(message.chat.id, 'Картинка добавлена в очередь.' , reply_markup=markup)
+        if message.text.strip() == '+':  # will make bug if moderator sent + without the context of moderating
+            insert_queue(
+                table='PostQueue',
+                user_id=user_id,
+                username=username,
+                image=image,
+                extra=extra
+            )
+            markup = make_buttons(message)
+            bot.send_message(
+                message.chat.id,
+                'Картинка добавлена в очередь.',
+                reply_markup=markup
+            )
 
         elif message.text.strip() == '-':
-            popqueue(table='SuggestionQueue', image=image)
-            markup = makebuttons(message)
-            bot.send_message(message.chat.id, 'Картинка снята с модерации.', reply_markup=markup)
+            pop_queue(table='SuggestionQueue', image=image)
+            markup = make_buttons(message)
+            bot.send_message(
+                message.chat.id,
+                'Картинка снята с модерации.',
+                reply_markup=markup
+            )
 
-        elif message.text.strip() == 'Пропуск': # remove image from queue and add it to the end
-            popqueue(table='SuggestionQueue', image=image)
-            insertqueue(table='SuggestionQueue', user_id=user_id, username=username, image=image, extra=extra)
-            markup = makebuttons(message)
-            bot.send_message(message.chat.id, 'Картинка перемещена в начало очереди.', reply_markup=markup)
+        elif message.text.strip() == 'Пропуск':  # remove image from queue and add it to the end
+            pop_queue(table='SuggestionQueue', image=image)
+            insert_queue(
+                table='SuggestionQueue',
+                user_id=user_id,
+                username=username,
+                image=image,
+                extra=extra
+            )
+            markup = make_buttons(message)
+            bot.send_message(
+                message.chat.id,
+                'Картинка перемещена в начало очереди.',
+                reply_markup=markup
+            )
 
 
 @bot.message_handler(content_types=['photo'])  # receive an image from user
@@ -169,7 +207,13 @@ def handle_photo(message):
     received_image = message.photo[-1].file_id
     logging.info(
         'A photo has just received from user {0}, id {1}'.format(message.from_user.first_name, message.from_user.id))
-    insertqueue(table='SuggestionQueue', user_id=message.from_user.id, username=message.from_user.username, image=received_image, extra='')
+    insert_queue(
+        table='SuggestionQueue',
+        user_id=message.from_user.id,
+        username=message.from_user.username,
+        image=received_image,
+        extra=''
+    )
     bot.send_message(message.chat.id, 'Отправил на проверку.')
 
 
